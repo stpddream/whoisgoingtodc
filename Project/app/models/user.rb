7fbf96fdd304abe88527e4f788b3157b5827fb1c
Panda <stmishu@gmail.com>
2014-01-25 00:08:10 -0500

    [Panda] Complete user sign up, sign in, sign out.

--- Project/app/models/user.rb
+++ Project/app/models/user.rb
@@ -2,8 +2,15 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+  
   validates :email, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   
+  private
+  
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
   
 end
