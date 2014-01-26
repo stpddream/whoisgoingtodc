af315e3e23a82e6004567c28ed27b915cdd6c41e
Panda <stmishu@gmail.com>
2014-01-26 03:17:59 -0500

    [Panda] Mobile gem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,25 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  def check_for_mobile
-    session[:mobile_override] = params[:mobile] if params[:mobile]
-    prepare_for_mobile if mobile_device?
-  end
+  include Mobylette::RespondToMobileRequests
 
-  def mobile_device?
-    if session[:mobile_override]
-      session[:mobile_override] == "1"
-    else    
-      (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !=~ /iPad/)
-    end
-  end
-  helper_method :mobile_device?
-  
-  def prepare_for_mobile
-    prepend_view_path Rails.root + 'app' + 'views_mobile'
-  end
-  
-  
   include SessionsHelper
 end
