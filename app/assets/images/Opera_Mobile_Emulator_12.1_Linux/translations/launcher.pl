2f3d0bba7bc4d0b840c86ee645ab7dcc1c69145e
jacyxli <jacy.xli@gmail.com>
2014-01-26 14:34:23 -0500

    DC to HCDC

---
+++ app/assets/images/Opera_Mobile_Emulator_12.1_Linux/translations/launcher.pl
@@ -0,0 +1,49 @@
+S_LANGUAGE Polski
+S_LANGUAGE_WORD Język
+S_CHANGE_LANGUAGE Zmień język
+S_CHOOSE_LANGUAGE Wybierz język
+S_LANGUAGE_KEEP_ASKING Pokazuj to okno podczas uruchamiania
+S_UNABLE_TO_START_LAUNCHER Nie można uruchomić programu uruchamiającego Opera Mobile Emulator.
+S_UNABLE_TO_READ_CONFIG Nie można odczytać pliku konfiguracyjnego Launcher.ini.
+S_PROFILE Profil
+S_ADD Dodaj
+S_REMOVE Usuń
+S_WINDOW_SCALE Skalowanie okna
+S_ARGUMENTS Argumenty
+S_FULL_BROWSER_RESET Przywróć ustawienia domyślne przed uruchomieniem.
+S_HELP Pomoc
+S_LAUNCH Uruchom
+S_ADD_PROFILE Utwórz profil
+S_ENTER_PROFILE_NAME Wpisz nazwę nowego profilu.
+S_SAVE_SAVE_AS Zapisz/Zapisz jako
+S_ENTER_NEW_PROFILE_NAME Wpisz nową nazwę profilu lub pozostaw bieżącą.
+S_SAVE_CHANGES Zapisać zmiany?
+S_PROFILE_ALREADY_EXISTS Profil %1 już istnieje. Czy chcesz go zastąpić?
+S_OK OK
+S_CANCEL Anuluj
+S_YES Tak
+S_NO Nie
+S_REMOVE_PROFILE_QUESTION Czy na pewno chcesz usunąć ten profil?
+S_CONFIRM_REMOVE_PROFILE Usuń profil
+S_NAME Nazwa
+S_WIDTH Szerokość
+S_HEIGHT Wysokość
+S_ADD_RESOLUTION Dodaj rozdzielczość
+S_RESOLUTION_TOO_BIG Za duża rozdzielczość
+S_RESOLUTION_SIZE_QUESTION Dodajesz rozdzielczość %1x%2, która jest większa od bieżącej rozdzielczości ekranu: %3x%4.\n Czy na pewno kontynuować?
+S_PIXEL_DENSITY Gęstość pikseli
+S_ADD_PIXEL_DENSITY Dodaj gęstość pikseli
+S_FAILED_TO_RESET_BROWSER Przywracanie ustawień domyślnych nie powiodło się – nie można usunąć %1.
+S_FAILED_START_EMULATOR Nie można uruchomić Opera Mobile Emulator
+S_EMULATOR_WARNING Ostrzeżenie
+S_CONFIG_OUT_OF_DATE_REPLACE Plik konfiguracyjny Launcher.ini jest nieaktualny i nie zostanie użyty. Jego nazwa została zmieniona na %1, aby zrobić miejsce dla nowego pliku konfiguracyjnego.
+S_CONFIG_OUT_OF_DATE_INFO Plik konfiguracyjny Launcher.ini jest nieaktualny.
+S_DEFAULT Domyślna
+S_OPTION_STILL_USED_BY_PROFILES Ta opcja jest używana w poniższych profilach. Czy na pewno chcesz kontynuować?
+S_CONFIRM_REMOVE Usuń
+S_WINDOW_SIZE_WILL_BE_LIMITED Rozmiar okna zostanie ograniczony przez dostępną rozdzielczość ekranu.
+S_RESOLUTION Rozdzielczość
+S_INPUT Interfejs użytkownika
+S_USER_AGENT_STRING User Agent
+S_WINDOW_SCALE_TOO_LOW Wybrany rozmiar okna jest mniejszy niż dopuszczalny.
+S_SELECT_HIGHER_SCALE Wybierz poziom skali wyższy niż %1.
---
+++ app/assets/images/Opera_Mobile_Emulator_12.1_Linux/translations/launcher.ru
@@ -0,0 +1,49 @@
+S_LANGUAGE Русский
+S_LANGUAGE_WORD Язык
+S_CHANGE_LANGUAGE Сменить язык
+S_CHOOSE_LANGUAGE Выберите язык
+S_LANGUAGE_KEEP_ASKING Показывать этот диалог при старте
+S_UNABLE_TO_START_LAUNCHER Невозможно запустить загрузчик Opera Mobile Emulator.
+S_UNABLE_TO_READ_CONFIG Невозможно прочитать файл конфигурации Launcher.ini.
+S_PROFILE Профиль
+S_ADD Добавить
+S_REMOVE Удалить
+S_WINDOW_SCALE Масштаб окна
+S_ARGUMENTS Аргументы
+S_FULL_BROWSER_RESET Полный сброс браузера при запуске
+S_HELP Справка
+S_LAUNCH Запустить
+S_ADD_PROFILE Добавить профиль
+S_ENTER_PROFILE_NAME Введите название нового профиля:
+S_SAVE_SAVE_AS Сохранить/Сохранить как…
+S_ENTER_NEW_PROFILE_NAME Введите новое имя профиля или оставьте текущее.
+S_SAVE_CHANGES Сохранить изменения?
+S_PROFILE_ALREADY_EXISTS Профиль %1 уже существует. Заменить?
+S_OK OK
+S_CANCEL Отмена
+S_YES Да
+S_NO Нет
+S_REMOVE_PROFILE_QUESTION Вы действительно хотите удалить данный профиль?
+S_CONFIRM_REMOVE_PROFILE Удаление профиля
+S_NAME Название
+S_WIDTH Ширина
+S_HEIGHT Высота
+S_ADD_RESOLUTION Добавить разрешение
+S_RESOLUTION_TOO_BIG Слишком высокое разрешение
+S_RESOLUTION_SIZE_QUESTION Вы запросили разрешение %1x%2, которое выше текущего разрешения экрана: %3x%4. Продолжить?
+S_PIXEL_DENSITY Плотность пикселей
+S_ADD_PIXEL_DENSITY Добавить плотность пикселей
+S_FAILED_TO_RESET_BROWSER Не удалось осуществить полный сброс браузера - невозможно удалить %1.
+S_FAILED_START_EMULATOR Не удалось запустить Opera Mobile Emulator
+S_EMULATOR_WARNING Предупреждение Opera Mobile Emulator Launcher
+S_CONFIG_OUT_OF_DATE_REPLACE Файл конфигурации Launcher.ini устарел и не будет использован. Вместо него создан новый файл. Старый файл переименован в %1.
+S_CONFIG_OUT_OF_DATE_INFO Файл конфигурации Launcher.ini устарел.
+S_DEFAULT По умолчанию
+S_OPTION_STILL_USED_BY_PROFILES Нижеследующие профили используют эти параметры. Продолжить?
+S_CONFIRM_REMOVE Удаление
+S_WINDOW_SIZE_WILL_BE_LIMITED Размер окна будет ограничен текущим разрешением экрана.
+S_RESOLUTION Разрешение
+S_INPUT Тип пользовательского интерфейса
+S_USER_AGENT_STRING Строка User Agent
+S_WINDOW_SCALE_TOO_LOW Задан размер окна меньше допустимого.
+S_SELECT_HIGHER_SCALE Пожалуйста, выберите масштаб окна не менее %1.
