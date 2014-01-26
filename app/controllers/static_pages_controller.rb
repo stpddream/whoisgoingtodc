bd2be5d7776750033d4bd45e6245c33212183ced
Panda <stmishu@gmail.com>
2014-01-25 22:45:29 -0500

    [Panda] Add tab for feeds.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
     
-    
 	if signed_in?
 		@event = current_user.events.build if signed_in?
   	@public_feed_items = Event.where(created_at: 1.hour.ago..Time.now, visibility: true).where.not(user_id: current_user.id)
 		@cur_user_friend = current_user.friendeds.all
 		@friend_feed_items = Event.where(id:nil)
-		@cur_user_friend.each{|friend| @friend_feed_items << Event.where(user_id: friend_id, created_at: 1.hour.ago..Time.now) }
+		@cur_user_friend.each { |friend| 
+		  Event.where(user_id: friend.id, created_at: 1.hour.ago..Time.now).each do |event|
+		    @friend_feed_items << event		    
+		    end 
+		}
 	else 
 	  @user = User.new
 	end
