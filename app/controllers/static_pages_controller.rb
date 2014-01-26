a917b30c13868cd1aca33df629b0a8c37f3dc91e
jacyxli <jacy.xli@gmail.com>
2014-01-26 04:43:09 -0500

    Updated Home and Profile UI

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     
 	if signed_in?
 		@event = current_user.events.build if signed_in?
-  	@public_feed_items = Event.where(created_at: 1.hour.ago..Time.now, visibility: true).where.not(user_id: current_user.id)
+      	@public_feed_items = Event.where(created_at: 1.hour.ago..Time.now, visibility: true).where.not(user_id: current_user.id)
 		@cur_user_friend = current_user.friendeds.all
 		@friend_feed_items = Event.where(id:nil)
 		@cur_user_friend.each { |friend| 
